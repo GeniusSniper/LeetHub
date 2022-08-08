9a0633948feb88ae59c4ab1ceabe9f6975e80e41
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-08-08 13:00:20 -0500

    Time: 100 ms (27.71%), Space: 42.1 MB (83.06%) - LeetHub

---
+++ 100-same-tree/100-same-tree.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    return !p || !q ? !p && !q : p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
