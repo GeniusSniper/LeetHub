77237a4ac69a5c08c7ba0e2962c54e8e58f2c7f9
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-08-10 14:49:04 -0500

    Time: 125 ms (84.89%), Space: 46.9 MB (62.52%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -3,31 +3,35 @@
  * @param {string} p
  * @return {number[]}
  */
-var findAnagrams = function(s, p) {
-    if(p.length > s.length) return [];
-    let answer = [], objP = objinput(p), objS = objinput(s.slice(0, p.length)), firstIndex = s[0];
-    if(isEqual(objS, objP)) answer.push(0);
-
-    for(let i = 1; i < s.length - p.length + 1; i++) {
-        objS[firstIndex] = objS[firstIndex] - 1;
-        firstIndex = s[i];
-        objS[s[i + p.length - 1]] = objS[s[i + p.length - 1]] ? objS[s[i + p.length - 1]] + 1 : 1;
-
-        if(isEqual(objS, objP)) answer.push(i);
+var findAnagrams = function(s, p) { 
+    const output = [];
+    const neededChars = {};
+    
+    for (let char of p) {
+        if (char in neededChars) {
+            neededChars[char]++
+        } else neededChars[char] = 1
     }
-
-    return answer;
+    
+    let left = 0;
+    let right = 0; 
+    let count = p.length 
+    
+    while (right < s.length) {
+        
+        if (neededChars[s[right]] > 0) count--;
+        
+        neededChars[s[right]]--;
+        right++;
+        
+        if (count === 0) output.push(left);
+        
+        if (right - left == p.length) {
+            if (neededChars[s[left]] >= 0) count++;
+            
+            neededChars[s[left]]++;
+            left++;
+        }
+    }
+    return output;
 };
-
-let isEqual = (a, b) => {
-    return !Object.keys(b).some( k => b[k] !== a[k]);
-}
-
-let objinput = (input) => {
-    let obj = {};
-    for(let i = 0; i < input.length; i++) {
-        obj[input[i]] = obj[input[i]] ? obj[input[i]] + 1 : 1;
-    }
-
-    return obj;
-}
