31752305cf96654c51e62cf020506fdb77751e86
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-08-10 14:40:32 -0500

    Time: 126 ms (94.68%), Space: 52.9 MB (76.85%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -5,19 +5,10 @@
 var groupAnagrams = function(strs) {
     let ans = {};
 
-    function findChar(str) {
-        let charC = '', obj = {};
-        for(let i = 0; i < str.length; i++) {
-            obj[str[i]] = obj[str[i]] ? obj[str[i]] + 1 : 1;
-        }
-
-        let arr = Object.keys(obj).sort( (a, b) => a < b ? 1 : -1);
-        for(let i = 0; i < arr.length; i++) charC += obj[arr[i]] + arr[i];
-        return charC;
-    }
-
-    for(let i = 0; i < strs.length; i++) {
-        let charC = findChar(strs[i]);
+    for (let i = 0; i < strs.length; i++) {
+        const letters = new Array(26).fill(0);
+        for (let j = 0; j < strs[i].length; j++) letters[strs[i][j].charCodeAt() - 97]++;
+        let charC = letters.join('#');
         ans[charC] ? ans[charC].push(strs[i]) : ans[charC] = [strs[i]];
     }
     return Object.values(ans);
