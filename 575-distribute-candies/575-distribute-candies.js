19442659545b8dc49df0cebc52dd6030018d5255
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-08-12 22:16:27 -0500

    Time: 483 ms (13.82%), Space: 62.8 MB (5.02%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.js
+++ 575-distribute-candies/575-distribute-candies.js
@@ -4,7 +4,7 @@
  */
 var distributeCandies = function(candyType) {
     let typesOfCandy = {};
-    for(let candy in candyType) typesOfCandy[candyType[candy]] = typesOfCandy[candyType[candy]] ? typesOfCandy[candyType[candy]] + 1 : 1;
+    for(let candy in candyType) typesOfCandy[candyType[candy]] = true;
     let candyLen = parseInt(candyType.length / 2), typeOfCandyLen = Object.keys(typesOfCandy).length;
     return typeOfCandyLen > candyLen ? candyLen : typeOfCandyLen;
 };
