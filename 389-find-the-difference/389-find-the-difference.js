ec02bfcbf15510a123aba30dabca0b05aad8f71e
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-08-12 23:04:50 -0500

    Time: 95 ms (66.75%), Space: 45.7 MB (17.43%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.js
+++ 389-find-the-difference/389-find-the-difference.js
@@ -5,10 +5,13 @@
  */
 var findTheDifference = function(s, t) {
     let difference = {};
-    for(let i in s) difference[s[i]] = difference[s[i]] ? difference[s[i]] + 1 : 1;
-    for(let i in t) {
-        difference[t[i]] -= 1;
+    for(let i in s) {
+        difference[s[i]] = difference[s[i]] ? difference[s[i]] + 1 : 1;
+        difference[t[i]] = difference[t[i]] ? difference[t[i]] - 1 : -1;
+        if(difference[s[i]] === 0) delete difference[s[i]];
         if(difference[t[i]] === 0) delete difference[t[i]];
     }
+    difference[t[s.length]] = difference[t[s.length]] ? difference[t[s.length]] - 1 : -1;
+    if(difference[t[s.length]] === 0) delete difference[t[s.length]] ;
     return Object.keys(difference)[0];
 };
