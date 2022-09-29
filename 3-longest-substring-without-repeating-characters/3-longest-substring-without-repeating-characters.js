ec3fd8f71bea753a8d82ea877677f890b6985f0f
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-09-29 16:03:23 -0500

    Time: 98 ms (92.09%), Space: 49.3 MB (23.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,10 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    const map = {};
-    var left = 0;
-    
+var lengthOfLongestSubstring = function(s, map = {}, left = 0) {
     return s.split('').reduce((max, v, i) => {
         left = map[v] >= left ? map[v] + 1 : left;
         map[v] = i;
