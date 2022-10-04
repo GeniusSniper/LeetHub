2d7435482969b6a945bb2c32742a20687eaaed7d
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-10-03 20:35:56 -0500

    Time: 2064 ms (5.01%), Space: 42.1 MB (90.20%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let count = 0;
+    for(let i = 0; i < s.length; i++) {
+        let cur = s[i], kc = k, c = 1;
+        for(let j = i + 1; j < s.length; j++) {
+            c++;
+            if(cur === s[j]) continue;
+            if(kc > 0) {
+                kc--;
+                continue;
+            }
+            c--;
+            break;
+        }
+        if(kc > 0) c = Math.min(s.length, c + kc);
+        count = Math.max(count, c);
+    }
+    return count;
+};
