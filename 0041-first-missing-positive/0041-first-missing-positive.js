56c41b63012bbfe0b03812c3df4c545a9885618f
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-10-26 19:00:19 -0500

    Time: 152 ms (29.15%), Space: 50.2 MB (57.38%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let one;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === 1) {
+            one = true;
+            break;
+        }
+    }
+    if(!one) return 1;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] < 1 || nums[i] > nums.length) nums[i] = 1;
+    }
+    
+    for(let i = 0; i < nums.length; i++) {
+        let cur = (nums[i] - 1) % nums.length;
+        nums[cur] += nums.length;
+    }
+    console.log(nums)
+    for(let i = 1; i < nums.length; i++) {
+        if(nums[i] <= nums.length) return i + 1;
+    }
+    
+    return nums.length + 1;
+};
