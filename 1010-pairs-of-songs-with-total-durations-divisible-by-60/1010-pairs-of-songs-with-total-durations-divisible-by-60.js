8a8f5a7c426c46c9c789df46df371b136585abae
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2022-11-15 15:24:41 -0600

    Time: 131 ms (43.55%), Space: 47.5 MB (12.10%) - LeetHub

---
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} time
+ * @return {number}
+ */
+var numPairsDivisibleBy60 = function(time) {
+    let cache = {}, count = 0;
+    for(let i = 0; i < time.length; i++) {
+        let tem = time[i] % 60;
+        if(cache[60 - tem] || tem === 0 && cache[tem]) count += cache[60 - tem] || cache[tem];
+        cache[tem] = cache[tem] ? cache[tem] + 1 : 1;
+    }
+    console.log(cache)
+    return count;
+};
