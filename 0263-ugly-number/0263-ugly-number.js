534ccaf08e62294e52b175afbfa8aa0f4a324db5
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2023-01-20 10:29:41 -0600

    Time: 75 ms (78.62%), Space: 42.7 MB (68.52%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.js
+++ 0263-ugly-number/0263-ugly-number.js
@@ -2,13 +2,11 @@
  * @param {number} n
  * @return {boolean}
  */
-var isUgly = function(n, cache = {}) {
-    if(cache[n]) return cache[n];
+var isUgly = function(n) {
     if(n === 0) return false;
-    if(n === 1 || n == 2 || n == 3 || n == 5) return true;
-    let div2 = n % 2 === 0 ? isUgly(n / 2) : false,
-        div3 = n % 3 === 0 ? isUgly(n / 3) : false,
-        div5 = n % 5 === 0 ? isUgly(n / 5) : false;
-    cache[n] = div2 || div3 || div5;
-    return cache[n];
+    if(n === 1) return true;
+    if(n % 2 === 0) return isUgly(n / 2);
+    if(n % 3 === 0) return isUgly(n / 3);
+    if(n % 5 === 0) return isUgly(n / 5);
+    return false;
 };
