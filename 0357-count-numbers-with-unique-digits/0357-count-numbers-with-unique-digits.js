e46741695354e3f673138ec1e7d4db7be92adeda
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2023-01-20 10:58:26 -0600

    Time: 51 ms (97.96%), Space: 42 MB (18.37%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.js
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.js
@@ -4,12 +4,10 @@
  */
 var countNumbersWithUniqueDigits = function(n) {
     if(n === 0) return 1;
-    if(n === 1) return 10;
-    if(n === 2) return 91;
-    if(n === 3) return 739;
-    if(n === 4) return 5275;
-    if(n === 5) return 32491;
-    if(n === 6) return 168571;
-    if(n === 7) return 712891;
-    if(n === 8) return 2345851;
+    let answer = 10, base = 9;
+    for(let i = 2; i < n + 1; i++) {
+        base = base * (9 - i + 2);
+        answer += base;
+    }
+    return answer;
 };
