2ea8d58915cc11933642d77f83dd5808c5d020fc
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2023-06-04 09:16:49 -0500

    Time: 65 ms (86.99%), Space: 46 MB (79.35%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -3,5 +3,29 @@
  * @return {string}
  */
 var jsonStringify = function(object) {
-    return JSON.stringify(object)
+    if(typeof object === 'string') return '"' + object + '"';
+    if(typeof object !== 'object') return object;
+    let result = '';
+    if(Array.isArray(object)){
+        result += '[';
+        for(let i = 0; i < object.length - 1; i++) {
+            result += jsonStringify(object[i]) + ',';
+        }
+        if(object.length > 0){
+            result += jsonStringify(object[object.length - 1]);
+        }
+        return result +']';
+    }
+    if(object === null) return null;
+    result += '{';
+    const arr = Object.keys(object);
+    for(let i = 0; i < arr.length - 1; i++){
+        result += '"' + arr[i] + '"' + ':';
+        result += jsonStringify(object[arr[i]]) + ',';
+    }
+    if(arr.length > 0) {
+        result += '"' + arr[arr.length - 1] + '":';
+        result += jsonStringify(object[arr[arr.length - 1]]);
+    }
+    return result + '}';
 };
