77f5153d6e644e978673009c8657fa097aac8185
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2023-06-09 14:44:32 -0500

    Time: 60 ms (48.39%), Space: 42 MB (48.39%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.js
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.js
@@ -4,17 +4,10 @@
  * @return {number}
  */
 var minMoves = function(target, maxDoubles) {
-    function helper(num = target, maxDouble = maxDoubles){
-        if(num === 1) return 0;
-        if(maxDouble === 0) return num - 1;
-        let doubleNum = Infinity, notDouble = Infinity;
-        if(num % 2 === 0 && maxDouble > 0) {
-            doubleNum = helper(num / 2, maxDouble - 1);
-        }
-        if(num % 2 === 1){
-            notDouble = helper(num - 1, maxDouble);            
-        }
-        return 1 + Math.min(doubleNum, notDouble);
+    if(target === 1) return 0;
+    if(maxDoubles === 0) return target - 1;
+    if(target % 2 === 0) {
+        return 1 + minMoves(target / 2, maxDoubles - 1);
     }
-    return helper();
+    return 1 + minMoves(target - 1, maxDoubles);
 };
