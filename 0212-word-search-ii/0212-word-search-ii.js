85810d188862139a71651159750dd1410c192b1c
Ti Wei <82611136+GeniusSniper@users.noreply.github.com>
2023-06-12 15:19:15 -0500

    Time: 5060 ms (15.15%), Space: 50 MB (52.13%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.js
@@ -0,0 +1,44 @@
+/**
+ * @param {character[][]} board
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(board, words) {
+    let dict = dictory(words), myset = new Set();
+    for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[i].length; j++) {
+            checkword(i, j);
+        }
+    }
+    function checkword(i, j, cache = {}, temp = dict){
+        if(i < 0 || j < 0 || i >= board.length || j >= board[i].length) return;
+        let word = temp[board[i][j]];
+        if(!word || cache[i + '#' + j]) return ;
+        cache[i + '#' + j] = true;
+        if(word['isword']) myset.add(word['isword']);
+        checkword(i + 1, j, cache, word);
+        checkword(i, j + 1, cache, word);
+        checkword(i - 1, j, cache, word);
+        checkword(i, j - 1, cache, word);
+        delete cache[i + '#' + j];
+    }
+    
+    return [...myset];
+};
+
+
+function dictory(arr) {
+    let obj = {};
+    for(let i = 0; i < arr.length; i++) {
+        let word = arr[i];
+        let temp = obj;
+        for(let j = 0; j < word.length; j++) {
+            if(!temp[word[j]]) {
+                temp[word[j]] = {};
+            }
+            temp = temp[word[j]];
+        }
+        temp['isword'] = word;
+    }
+    return obj;
+}
